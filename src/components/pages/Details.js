9cec8e793f71a90ee714ce3ce7bb7287da85b792
Swathihanumanthappa123 <swathiattigunda@gmail.com>
2024-05-08 16:25:35 -0700

    comitted files

---
+++ src/components/pages/Details.js
@@ -0,0 +1,24 @@
+// Details.js
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+
+const Details = ({ formData, onNextClick}) => {
+    const handleNextClick = () => {
+        onNextClick(); // Call the function provided by the parent component
+      };
+  return (
+    <div>
+      <p>Name: {formData.name}</p>
+      <p>Skills: {formData.skills}</p>
+      <p>Job Title: {formData.jobTitle}</p>
+      <p>Current Company: {formData.company}</p>
+      <p>Current Position: {formData.position}</p>
+      <Button variant="primary" onClick={handleNextClick}>
+              Next
+       </Button>
+    </div>
+  );
+};
+
+export default Details;
