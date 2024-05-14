5573c16ddcd88f0384e612c11a62a39b67699148
PJ Fenton <patrick.fenton.4.19@gmail.com>
2024-05-14 15:40:16 -0400

    Commented Out Reduntant form

--- src/components/ApplicantForm/ApplicationData.jsx
+++ src/components/ApplicantForm/ApplicationData.jsx
@@ -1,43 +1,43 @@
-//ApplicantData.js
+// //ApplicantData.js
 
-import React from 'react';
+// import React from 'react';
 
-function ApplicantData({ data, onEdit, onDelete }) {
-   if (!data) {
-       return <div>No data to display</div>;
-   }
+// function ApplicantData({ data, onEdit, onDelete }) {
+//    if (!data) {
+//        return <div>No data to display</div>;
+//    }
 
-   const handleEdit = () => {
-       // Call the onEdit function with the applicant data
-       onEdit(data);
-   };
+//    const handleEdit = () => {
+//        // Call the onEdit function with the applicant data
+//        onEdit(data);
+//    };
 
-   const handleDelete = () => {
-       // Call the onDelete function with the applicant data
-       onDelete(data);
-   };
+//    const handleDelete = () => {
+//        // Call the onDelete function with the applicant data
+//        onDelete(data);
+//    };
 
-   return (
-       <div>
-           <h2>Applicant Information</h2>
-           <p>Name: {data.name}</p>
-           {data.name && (
-               <>
-                   <p>Skills: {data.skills}</p>
-                   <p>Job Title: {data.jobTitle}</p>
-                   <p>Current Company: {data.company}</p>
-                   <p>Current Position: {data.position}</p>
-               </>
-           ) }
+//    return (
+//        <div>
+//            <h2>Applicant Information</h2>
+//            <p>Name: {data.name}</p>
+//            {data.name && (
+//                <>
+//                    <p>Skills: {data.skills}</p>
+//                    <p>Job Title: {data.jobTitle}</p>
+//                    <p>Current Company: {data.company}</p>
+//                    <p>Current Position: {data.position}</p>
+//                </>
+//            ) }
 
-           <button type="button" className="btn btn-success" onClick={handleEdit}>
-               Edit
-           </button>
-           <button type="button" className="btn btn-danger" onClick={handleDelete}>
-               Delete
-           </button>
-       </div>
-   );
-}
+//            <button type="button" className="btn btn-success" onClick={handleEdit}>
+//                Edit
+//            </button>
+//            <button type="button" className="btn btn-danger" onClick={handleDelete}>
+//                Delete
+//            </button>
+//        </div>
+//    );
+// }
 
-export default ApplicantData;
+// export default ApplicantData;
